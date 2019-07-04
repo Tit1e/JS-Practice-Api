7e62065a3f0e734506f6df1e1b40bd927f79ad28
Tit1e <evollyone@outlook.com>
2019-07-04 13:32:48 +0800

    引入题目相关模块

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app, pool } = require("./connent")
 const common = require("./modules/api/common")
 const user = require("./modules/api/user")
+const subjects = require("./modules/api/subjects")
 
 app.all("*", (req, res, next) => {
   next()
@@ -9,7 +10,7 @@ app.all("*", (req, res, next) => {
 app.all("/", (req, res) => {
 })
 
-app.use("/api", common, user)
+app.use("/api", common, user, subjects)
 
 app.listen(3000, () => {
   console.log("服务在 3000 端口启用")
